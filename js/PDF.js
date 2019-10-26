37d5a0d99e81e736f8c62441ff3270df9f80a06d
Alejandro García <45419684+mediadocena@users.noreply.github.com>
2019-10-26 12:15:46 +0200

    Funcionalidad pdf

--- js/PDF.js
+++ js/PDF.js
@@ -4,9 +4,6 @@ function Imprimir() {
     var nombre1 = document.getElementById("nombreText").value;
     var nombre2 = document.getElementById("mi id").value;
     pdf.text(20, 10, "Nombres:");
-    pdf.text(20, 20, nombre1);
-    pdf.text(20, 30, nombre2);
-    pdf.autoTable({ html: '#mitabla' });
     pdf.autoTable({
         head: [['Name', 'Email', 'Country']],
         body: [
