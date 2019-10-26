78568529e60805551435756e2982db1aa859a311
Alejandro García <45419684+mediadocena@users.noreply.github.com>
2019-10-26 11:56:53 +0200

    tabla hecha

--- js/PDF.ts
+++ js/PDF.ts
@@ -5,6 +5,15 @@ function Imprimir() {
     pdf.text(20,10,"Nombres:");
     pdf.text(20,20,nombre1);
     pdf.text(20,30,nombre2);
+    pdf.autoTable({html:'#mitabla'});
+    pdf.autoTable({
+      head: [['Name', 'Email', 'Country']],
+      body: [
+          ['David', 'david@example.com', 'Sweden'],
+          ['Castille', 'castille@example.com', 'Norway'],
+          // ...
+      ]
+  });
     pdf.save("miPDF.pdf");
   };
   
